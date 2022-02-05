136ce112f51c7281d3411aa90da5af0935295a97
Leon Turpin <turpinleon@gmail.com>
2022-02-05 22:55:43 +0800

    It works! Kinda...

--- OndesController.ino
+++ OndesController.ino
@@ -8,10 +8,11 @@ struct Pulley {
 };
 
 Pulley pulley1 = { A0, 0, 0 };
+int currentNote = 60; // C
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(38400);
+  // Configure baud for midi
+  Serial.begin(31250);
 
   pulley1.angle = analogReadAngle(A0);
 }
@@ -23,12 +24,17 @@ void loop() {
   pulley1.angle = newAngle;
   pulley1.distance += distanceDelta;
 
+  int newNote = 60 + (pulley1.distance / 100.f);
+
   if (Serial.availableForWrite() > 32) {
-    Serial.print("d: \t");
-    Serial.print(pulley1.distance);
-    Serial.print("\t a:");
-    Serial.print(pulley1.angle);
-    Serial.print("\n");
+    // TODO: Debounce note changes.
+    if (newNote != currentNote) {
+      midiCommand(0x90, newNote, 0x64); // Start new note
+      midiCommand(0x80, currentNote, 0x64); // Stop current note
+      currentNote = newNote;
+    }
+    // TODO: This doesn't handle negative values properly
+    floatToPitchBend(((int)pulley1.distance % 100) / 400.f);
   }
 }
 
@@ -46,4 +52,22 @@ float differenceBetweenAngles(float angleStart, float angleEnd) {
   }
 
   return (minAngle == angleStart) ? diff : -diff;
+}
+
+// floatBend should be in the range -1 to 1
+void floatToPitchBend(float floatBend) {
+  int rawIntBend = floatBend * 0x3FFF; // 14 bits
+
+  int leastSignificatByte = rawIntBend & 0x7F;
+  int mostSignificatByte = (rawIntBend >> 7) & 0x7F;
+
+  midiCommand(0xE0, leastSignificatByte, mostSignificatByte);
+}
+
+// plays a MIDI note. Doesn't check to see that cmd is greater than 127, or that
+// data values are less than 127:
+void midiCommand(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
 }
