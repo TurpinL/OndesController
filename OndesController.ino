34160b3277b94f598ac711de00b9a652d3d2fcc8
Leon Turpin <turpinleon@gmail.com>
2022-08-12 22:10:00 +0800

    Mild correction for pulley size differences

--- OndesController.ino
+++ OndesController.ino
@@ -13,10 +13,11 @@
 #define READ_DATA_LO 0x00
 #define READ_DATA_HI 0x01
 
-const float MAX_RAW_VALUE = 4096;
+const int MAX_RAW_VALUE = 4096;
 const float RAW_TO_ANGLE = 360 / (float)MAX_RAW_VALUE;
-const float PULLEY_CIRCUMFERENCE_MM = 52.63;
-const float DISTANCE_BETWEEN_PULLEYS_MM = 395;
+const float LEFT_PULLEY_CIRCUMFERENCE_MM = 52.65; 
+const float RIGHT_PULLEY_CIRCUMFERENCE_MM = 52.45;
+const float DISTANCE_BETWEEN_PULLEYS_MM = 400;
 const float MM_PER_NOTE = 10;
 
 const byte PORTAMENTO = 5;
@@ -34,9 +35,9 @@ const byte LFOINT = 26;
 const int patchInitPin = 7;
 
 TwoWire Wire2(
-  &sercom2, 
-  4, // SDA 
-  3  // SCL
+  &sercom2,
+  4,  // SDA
+  3   // SCL
 );
 
 struct Pulley {
@@ -44,17 +45,36 @@ struct Pulley {
   float angle;
   float travel;
   int travelDirection;
+  float circumference;
 };
 
 float touche = 0.f;
 
 // Assumes the player's finger starts in the dead center
-Pulley pulleyLeft = { Wire, 0, DISTANCE_BETWEEN_PULLEYS_MM / 2, -1};
-Pulley pulleyRight = { Wire2, 0, DISTANCE_BETWEEN_PULLEYS_MM / 2, 1};
+Pulley pulleyLeft = { 
+  Wire, 
+  0, 
+  DISTANCE_BETWEEN_PULLEYS_MM / 2, 
+  1, 
+  LEFT_PULLEY_CIRCUMFERENCE_MM 
+};
+
+Pulley pulleyRight = { 
+  Wire2, 
+  0, 
+  DISTANCE_BETWEEN_PULLEYS_MM / 2, 
+  -1, 
+  RIGHT_PULLEY_CIRCUMFERENCE_MM 
+};
+
 int currentNote = -1;
 
 void setup() {
   SerialUSB.begin(115200);
+
+  while (!SerialUSB) {}
+  SerialUSB.println("Serial Ready");
+
   // Configure baud for midi
   Serial1.begin(31250);
 
@@ -66,11 +86,15 @@ void setup() {
 
   Wire2.begin();
   Wire2.setClock(I2C_FAST_MODE);
-  pinPeripheral(4, PIO_SERCOM_ALT); // Assign pins 4 & 3 to SERCOM functionality
+  pinPeripheral(4, PIO_SERCOM_ALT);  // Assign pins 4 & 3 to SERCOM functionality
   pinPeripheral(3, PIO_SERCOM_ALT);
 
+  SerialUSB.println("Initializing Left Pulley");
   initializePulley(pulleyLeft);
+  SerialUSB.println("Initializing Right Pulley");
   initializePulley(pulleyRight);
+
+  SerialUSB.println("Ready!");
 }
 
 void loop() {
@@ -82,39 +106,38 @@ void loop() {
   // Calculate X and Y offset of the player's finger
   float spoofDistBetweenPulleys = min(
     DISTANCE_BETWEEN_PULLEYS_MM,
-    pulleyLeft.travel + pulleyRight.travel 
+    pulleyLeft.travel + pulleyRight.travel
   );
 
   float s = 0.5 * (
-    pulleyLeft.travel
+    pulleyLeft.travel 
     + pulleyRight.travel 
     + spoofDistBetweenPulleys
-  ); // Semiperimiter
+  );  // Semiperimiter
 
   float y = sqrt(
-    (s - spoofDistBetweenPulleys) 
-    * (s - pulleyLeft.travel)
-    * (s - pulleyRight.travel)
-    * s
-  ) * 2 / spoofDistBetweenPulleys;
+              (s - spoofDistBetweenPulleys)
+              * (s - pulleyLeft.travel)
+              * (s - pulleyRight.travel)
+              * s)
+            * 2 / spoofDistBetweenPulleys;
 
-  float x = sqrt(
-    (pulleyLeft.travel * pulleyLeft.travel) - (y * y)
-  ) - (DISTANCE_BETWEEN_PULLEYS_MM / 2);
+  float x = sqrt((pulleyLeft.travel * pulleyLeft.travel) - (y * y))
+            - (DISTANCE_BETWEEN_PULLEYS_MM / 2);
 
-  float semitoneOffset = x / MM_PER_NOTE; 
+  float semitoneOffset = x / MM_PER_NOTE;
   int roundedSemitoneOffset = round(semitoneOffset);
 
-  // Start at C3 (1 octave below middle C) 
+  // Start at C3 (1 octave below middle C)
   int nextNote = 60 + roundedSemitoneOffset;
 
-  float sustain = log10(1 + touche * 9);
-  // float sustain = touche;
+  // float sustain = log10(1 + touche * 9);
+  float sustain = touche;
 
   if (Serial1.availableForWrite() > 32) {
     if (nextNote != currentNote) {
-      midiCommand(0x90, nextNote, 0x64); // Start new note
-      midiCommand(0x80, currentNote, 0x64); // Stop current note
+      midiCommand(0x90, nextNote, 0x64);     // Start new note
+      midiCommand(0x80, currentNote, 0x64);  // Stop current note
 
       currentNote = nextNote;
     }
@@ -123,17 +146,18 @@ void loop() {
 
     floatToPitchBend(bendSemitones);
     floatToCC(SUSTAIN, sustain);
-    floatToCC(LFORATE, y / 45.f);
-    floatToCC(LFOINT, y / 15.f);
+    // floatToCC(LFORATE, y / 45.f);
+    // floatToCC(LFOINT, y / 15.f);
   }
 
   if (SerialUSB.availableForWrite() > 32) {
+    SerialUSB.print("(");
     SerialUSB.print(currentNote);
-    SerialUSB.print(", (");
+    SerialUSB.print(", x:");
     SerialUSB.print(x);
-    SerialUSB.print(", ");
+    SerialUSB.print(", y:");
     SerialUSB.print(y);
-    SerialUSB.print(", ");
+    SerialUSB.print(", z:");
     SerialUSB.print(sustain);
     SerialUSB.println(")");
   }
@@ -146,7 +170,7 @@ float readAngle(TwoWire &wire) {
 int readRawAngle(TwoWire &wire) {
   wire.beginTransmission(AMS5600_ADDRESS);
   wire.write(READ_RAW_ANGLE_LO);
-  wire.endTransmission(); 
+  wire.endTransmission();
   wire.requestFrom(AMS5600_ADDRESS, 1);
 
   while (wire.available() == 0) {}
@@ -155,7 +179,7 @@ int readRawAngle(TwoWire &wire) {
 
   wire.beginTransmission(AMS5600_ADDRESS);
   wire.write(READ_RAW_ANGLE_HI);
-  wire.endTransmission(); 
+  wire.endTransmission();
   wire.requestFrom(AMS5600_ADDRESS, 1);
 
   while (wire.available() == 0) {}
@@ -171,7 +195,7 @@ float readTouche() {
 
   Wire.beginTransmission(AS5510_ADDRESS);
   Wire.write(READ_DATA_LO);
-  Wire.endTransmission(); 
+  Wire.endTransmission();
   Wire.requestFrom(AS5510_ADDRESS, 2);
 
   if (Wire.available() >= 2) {
@@ -200,7 +224,7 @@ float differenceBetweenAngles(float angleStart, float angleEnd) {
 // Assumes +/- 2 semitone pitchbend range on the synth. So the semitones param should be within that range
 void floatToPitchBend(float semitones) {
   // Map the +/- 2 semitone range to the 0 - 16,383 (14 bits) of midi's pitchbend message
-  int rawIntBend = (semitones + 2) * 0xFFF/* 12 bits */;
+  int rawIntBend = (semitones + 2) * 0xFFF /* 12 bits */;
 
   int leastSignificatByte = rawIntBend & 0x7F;
   int mostSignificatByte = (rawIntBend >> 7) & 0x7F;
@@ -209,8 +233,8 @@ void floatToPitchBend(float semitones) {
 }
 
 void floatToCC(byte ccAddress, float fraction) {
-  int cutoff = constrain(fraction, 0, 1) * 0x7F/* 7 bits */;
-  
+  int cutoff = constrain(fraction, 0, 1) * 0x7F /* 7 bits */;
+
   midiCommand(0xB0, ccAddress, cutoff);
 }
 
@@ -230,8 +254,7 @@ void initSynthPatch() {
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
 
-  if (interrupt_time - last_interrupt_time > 200) 
-  {
+  if (interrupt_time - last_interrupt_time > 200) {
     last_interrupt_time = interrupt_time;
 
     floatToCC(ATTACK, 0.f);
@@ -253,8 +276,8 @@ void updatePulley(Pulley &pulley) {
   float angleDelta = differenceBetweenAngles(pulley.angle, newAngle);
 
   pulley.angle = newAngle;
-  pulley.travel -= angleDelta 
-    / 360.0 
-    * PULLEY_CIRCUMFERENCE_MM 
-    * pulley.travelDirection;
+  pulley.travel += angleDelta
+                   / 360.0
+                   * pulley.circumference
+                   * pulley.travelDirection;
 }
